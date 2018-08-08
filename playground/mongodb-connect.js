83e7560ae66b087a2cac948e26615a3f5b50d159
Ema Myrtollari <ema.myrtollari@moreone.tech>
2018-08-08 10:33:17 +0200

    first commit

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,46 +1,35 @@
-//const MongoClient = require('mongodb').MongoClient;
-const { MongoClient, ObjectID } = require('mongodb');
+const {MongoClient}= require ('mongodb');
 
-var obj = new ObjectID();
-console.log(obj);
-//var user = { name: 'andrew', age: 25 };
-//var { name } = user;
-//console.log(name);
-
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if (err) {
-        console.log('Unable to connect to MongoDB server');
-
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+    if(err) {
+        return console.log('Unable to connect to the MongoDb server');
     }
-    console.log('Connected to MongoDB server');
+    console.log('Connected to the MongoDb server');
 
+    // db.collection('Todos').insertOne(
+    //     {
+    //         text:'My first todo', 
+    //         completed: false
+    //     },
+    //     (err,result)=>{
+    //         if(err){
+    //             return console.log('Insertion failed');
+    //         }
+    //     console.log(JSON.stringify(result.ops,undefined,2));
+    //     }
+    // );
 
-    //db.collection('Todos').insertOne({
-    //    text: 'Something to do',
-    //    completed: false
-    //}, (err, result) => {
-    //    if (err) {
-    //        return console.log('Unable to insert todo', err);
-    //    }
-    //    console.log(JSON.stringify(result.ops, undefined, 2));
-
-    //});
-
-//    db.collection('Users').insertOne({
-        
-//        name: 'Ema',
-//        age: 22,
-//        location: 'Tirana'
-//    }, (err, result) => {
-//        if (err) {
-//            return console.log('Unable to insert user', err);
-//        }
-//        console.log(result.ops);
-
+    //Insert new doc into Users, (mane,age,location)
+    db.collection('Users').insertOne({
+        name:'Ema',
+        age:22,
+        location: 'Tirana'
+    },(err,result)=>{
+        if(err){
+            return console.log('Unable to insert new user data');
+        }
+        console.log(JSON.stringify(result.ops,undefined,2));
+    });
     
-//});
-
-    //Insert new doc into Users collection (name, age,location)
-       db.close(); //closes the connection to the db
+    db.close();
 });
-
--- server/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.promise = global.promise; //set up to use promises
-mongoose.connect(process.env.MONGODB_URI)
-
-module.exports = { mongoose };
-
-//process.env.NODE_ENV ==='production'
